60d6f73a9dce7b7448837aa101c374d69078b474
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-18 23:50:27 +0600

    Add files via upload

---
+++ Chef in his Office/Chef in his Office.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,t;
+    cin>>t;
+    int week;
+    for(int i=0; i<t; i++){
+        cin>>x>>y;
+        week=4*x+y;
+        cout<<week<<endl;
+    }
+
+}
+
