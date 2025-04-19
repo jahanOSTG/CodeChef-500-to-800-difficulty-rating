2815ee2b25c4b80262d63945d44beb887d2bc7ab
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-19 15:32:43 +0600

    Add files via upload

---
+++ Car or Bike/Car or Bike.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x>>y;
+        if(x>y){
+            cout<<"CAR"<<endl;
+        }
+        else if(x==y){
+            cout<<"SAME"<<endl;
+        }
+        else{
+            cout<<"BIKE"<<endl;
+        }
+    }
+}
