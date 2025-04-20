a3fdb0143cc086cdf02dbd86c0a90b973e19805f
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-21 01:42:31 +0600

    Add files via upload

---
+++ Police and Thief/Police and Thief.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+            cin>>x>>y;
+           int dis=abs(x-y);
+            cout<<dis<<endl;
+
+    }
+
+}
