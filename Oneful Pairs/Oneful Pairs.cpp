cbd767c5dbb99e966939a27f9ff80093d730010d
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-22 00:00:07 +0600

    Add files via upload

---
+++ Oneful Pairs/Oneful Pairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+	// your code goes here
+	int result=a+b+(a*b);
+	if(result==111){
+            cout<<"Yes";
+
+	}else{
+	    cout<<"No";
+	}
+	return 0;
+}
