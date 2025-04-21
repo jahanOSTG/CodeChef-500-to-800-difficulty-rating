27b30e5042ac44083fb5b669e9359294934a835b
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-21 21:27:48 +0600

    Add files via upload

---
+++ Best of Two/Best of Two.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b,int c){
+    if(a<=b && a<=c){
+        return b+c;
+    }
+    else if(b<=a && b<=c){
+        return a+c;
+    }else{
+        return a+b;
+
+    }
+}
+int main(){
+    int t,a1,a2,a3,b1,b2,b3;
+    cin>>t;
+    for(int i=0; i<t; i++){
+            cin>>a1>>a2>>a3>>b1>>b2>>b3;
+       int alice=sum(a1,a2,a3);
+       int bob=sum(b1,b2,b2);
+    if(alice>bob){
+        cout<<"Alice"<<endl;
+    }else if(alice==bob){
+        cout<<"Tie"<<endl;
+
+    }else{
+        cout<<"Bob"<<endl;
+
+    }
+
+
+    }
+
+}
