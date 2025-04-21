c9a33462f5d44a88b08e23b41948e478ab90dce0
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-21 08:34:02 +0600

    Add files via upload

---
+++ Bus Seat Numbering/Bus Seat Numbering.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x;
+        if(x>=1 && x<=10){
+                cout<<"Lower Double"<<endl;
+
+        }else if(x>=11 && x<=15){
+             cout<<"Lower Single"<<endl;
+
+
+        }else if(x>=16 && x<=25){
+            cout<<"Upper Double"<<endl;
+        }else{
+             cout<<"Upper Single"<<endl;
+
+        }
+    }
+}
