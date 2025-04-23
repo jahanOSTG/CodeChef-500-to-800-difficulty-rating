6b3bc6dbe67bc32f3dbf10d3fe4209174dcac694
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-23 23:18:20 +0600

    Add files via upload

---
+++ Finding Shoes/Finding Shoes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,n,m;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>n>>m;
+        if(n<=m){
+            cout<<n<<endl;
+        }else{
+            cout<<(n-m)+n<<endl;
+        }
+    }
+}
