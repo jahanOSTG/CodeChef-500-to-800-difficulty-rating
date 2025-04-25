921981bb8eb81ce643caf0edfe18b34ba003392a
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-25 09:11:32 +0600

    Add files via upload

---
+++ Nearest Exit/Nearest Exit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x;
+        if(x>=1 && x<=50){
+            cout<<"LEFT"<<endl;
+        }else{
+            cout<<"RIGHT"<<endl;
+        }
+    }
+
+}
