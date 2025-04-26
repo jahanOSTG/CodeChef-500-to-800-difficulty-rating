5d39c56e8551edf6c3548bb9378a2a030092ff75
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-26 08:58:07 +0600

    Add files via upload

---
+++ Expert Setter/Expert Setter.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t, x, y;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x>>y;
+        if(y*2>=x){
+            cout<<"YES"<<endl;
+        }
+
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
