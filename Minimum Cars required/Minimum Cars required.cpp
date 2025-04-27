df6e4c2e4b5c42fe3003547ee80e2d9767956f64
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-27 08:31:21 +0600

    Add files via upload

---
+++ Minimum Cars required/Minimum Cars required.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>x;
+        int y=(x+3)/4;
+        cout<<y<<endl;
+    }
+}
