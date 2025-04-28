6b02dd8442c200116796e1f27d2923b046581bbb
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-28 08:40:28 +0600

    Add files via upload

--- Greater Average/Greater Average.cpp
+++ Greater Average/Greater Average.cpp
@@ -1,21 +1,22 @@
-
 #include<iostream>
 using namespace std;
-int main(){
-    int a,b,t;
 
-    float avg,c;
-    cin>>t;
-    for(int i=0; i<t; i++){
-        cin>>a>>b>>c;
-        avg=(a+b)/2.00;
-        if(avg>c){
-            cout<<"YES"<<endl;
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        double avg = (a + b) / 2.0;
+
+        if(avg > c) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
-        else{
-        cout<<"NO"<<endl;
-
     }
 
-  }
+    return 0;
 }
