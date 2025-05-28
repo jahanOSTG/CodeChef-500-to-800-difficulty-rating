4aa27da463c2a8cbb728f1181592b9e8282493b6
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-28 11:28:23 +0600

    Add files via upload

---
+++ Finding Square Roots/Finding Square Roots.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int t,a;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a;
+        int b=sqrt(a);
+        cout<<b<<endl;
+    }
+    return 0;
+}
