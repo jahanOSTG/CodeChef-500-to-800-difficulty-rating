8468dbc5c9597ea09e3962655a22b5a752e73b6f
Faria Jahan Janie <janiefaria315@gmail.com>
2025-09-15 19:41:19 +0600

    Create Valentine Gifts.cpp

---
+++ Valentine Gifts.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,t;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x>=127){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
